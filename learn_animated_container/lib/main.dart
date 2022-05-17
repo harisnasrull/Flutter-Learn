d7aa4a2a4c7fdc9206fe7e2372111ad61376b06e
harisnasrull <harisnasrullah212@gmail.com>
2022-05-17 10:56:32 +0700

    update animated container and gesture detector

---
+++ learn_animated_container/lib/main.dart
@@ -0,0 +1,45 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Random random = Random();
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Latihan Animated Container"),
+        ),
+        body: Center(
+          child: GestureDetector(
+            onTap: () {
+              setState(() {});
+            },
+            child: AnimatedContainer(
+              duration: Duration(seconds: 1),
+              width: 70.0 + random.nextInt(200),
+              height: 70.0 + random.nextInt(200),
+              color: Color.fromARGB(255, random.nextInt(255),
+                  random.nextInt(255), random.nextInt(255)),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
